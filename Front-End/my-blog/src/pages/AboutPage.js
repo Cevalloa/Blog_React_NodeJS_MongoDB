cc47c85bb4cb89645d8fd109b94157030bc4cea3
Alex Cevallos <acevallos@linkedin.com>
2020-10-15 09:34:27 -0700

    Adding routes for about, articles and article pages

---
+++ Front-End/my-blog/src/pages/AboutPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AboutPage = () => (
+    <React.Fragment>
+        <h1>About Me</h1>
+        <p>This is the body of my blog</p>
+    </React.Fragment>
+
+)
+
+export default AboutPage;
