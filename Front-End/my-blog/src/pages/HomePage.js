d379b5412a986b4353cc4860a52ba80368e0e796
Alex Cevallos <acevallos@linkedin.com>
2020-10-15 08:55:38 -0700

    Using react-router-dom and created initial blog page

---
+++ Front-End/my-blog/src/pages/HomePage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HomePage = () => (
+    <React.Fragment>
+        <h1>Hello, welcome to my blog!</h1>
+        <p>This is the body of my blog</p>
+    </React.Fragment>
+
+)
+
+export default HomePage;
