27c8d41019b6e41da5f4b278d74de04defc7779a
Alex Cevallos <acevallos@linkedin.com>
2020-10-17 18:15:49 -0700

    Created new route to add comments to posts

--- Back-End/Back-end-blog/src/server.js
+++ Back-End/Back-end-blog/src/server.js
@@ -3,13 +3,16 @@ import bodyParser from 'body-parser';
 
 const articlesInfo = {
     'improving-your-profile' : {
-        upvotes: 0
+        upvotes: 0,
+        comments: []
     },
     'learn-more' : {
-        upvotes: 0
+        upvotes: 0,
+        comments: []
     },
     'my-thoughts-on-resumes' : {
-        upvotes: 0
+        upvotes: 0,
+        comments: []
     }
 }
 
@@ -26,6 +29,14 @@ app.post('/api/articles/:name/upvote', (req, res) => {
 
     articlesInfo[articleName].upvotes += 1;
     res.status(200).send(`${articleName} now has ${articlesInfo[articleName].upvotes} upvotes`)
+})
+
+app.post('/api/articles/:name/add-comment', (req, res) => {
+    const { username, text} = req.body;
+    const articleName = req.params.name;
+
+    articlesInfo[articleName].comments.push({username, text})
+    res.status(200).send(articlesInfo[articleName]);
 })
 
 app.listen(8000, () => console.log('Listening on port 8000'));
