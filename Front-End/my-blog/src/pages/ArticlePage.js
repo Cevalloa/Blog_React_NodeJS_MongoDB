58e7a86b61bebf5c3ffa930eb7a242bc35c2166d
Alex Cevallos <acevallos@linkedin.com>
2020-10-19 20:08:24 -0700

    Using useEffect only when the article name from the url changes

--- Front-End/my-blog/src/pages/ArticlePage.js
+++ Front-End/my-blog/src/pages/ArticlePage.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, {useState, useEffect } from 'react';
 import articleContent from './article-content';
 
 const ArticlePage = ( { match }) => {
+
     const name = match.params.name;
     const article = articleContent.find(article => article.name === name);
+
+    const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
+
+    useEffect(() => {
+        setArticleInfo({ upvotes: Math.ceil(Math.random() * 10)});
+    }, [name])
 
     if(!article) return <h1>Article does not exist!</h1>
 
     return (
     <React.Fragment>
         <h1>{article.title}</h1>
+        <p>This post has been upvoted {articleInfo.upvotes} times</p>
         {article.content.map((paragraph, key) => (
             <p key = {key}>{paragraph}</p>
         ))}
